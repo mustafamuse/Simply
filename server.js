2219f72f9d66ea5533ddb7e17e39c4fdc3168127
Mustafa Muse <umpp101@icloud.com>
2020-04-21 12:49:52 -0700

    index route and page up and runnign

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
 const express = require("express")
-
 const app = express()
 
+
+app.set("view engine", "ejs")
+
+app.get("/", (request,response) =>{
+    response.render("index")
+})
 app.listen(process.env.PORT || 5000);
