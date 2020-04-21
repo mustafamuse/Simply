c7af0bea58fd5590cf82f6fe96bb14f46026d40f
Mustafa Muse <umpp101@icloud.com>
2020-04-21 15:23:42 -0700

    styling

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 const shortenedUrls = require("./models/shortenedUrls")
 const app = express()
 
-mongoose.connect( process.env.MONGO_URL || "mongodb://localhost/bitly_Clone", {
+mongoose.connect( process.env.MONGO_URL || "mongodb://localhost/Simply", {
   useNewUrlParser: true, useUnifiedTopology: true
 })
 .then(() => console.log('MongoDB Connected...'))
