1bb5e384bceec1451cc6804c93987343c9d6079c
Mustafa Muse <umpp101@icloud.com>
2020-04-21 13:51:14 -0700

    fixed error for mongoose

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require("express")
 const mongoose = require("mongoose")
+const shortenedUrls = require("./models/shortenedUrls")
 const app = express()
 
 mongoose.connect("mongodb://localhost/bitlyClone", {
     useNewUrlParser: true, useUnifiedTopology: true
 })
+.then(() => console.log('MongoDB Connected...'))
+.catch((err) => console.log(err))
+
 
 app.set("view engine", "ejs")
+app.use(express.urlencoded({ extended: false})) // so i can access the params from my request
 
 app.get("/", (request,response) =>{
     response.render("index")
 })
 
-app.post("/shortenedUrls", (request, response) =>{
-
+app.post("/shortenedUrls", async (request, response) =>{
+await shortenedUrls.create({ 
+    fullUrl: request.body.fullUrl
+})
+response.redirect("/")
 })
 app.listen(process.env.PORT || 5000);
